8ede9c83d532255f1b2aa66e2183be09a6229071
Adeik <15350389+vrbouza@users.noreply.github.com>
2019-03-15 19:30:10 +0100

    Now using ciencias instead of cienciasrw

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -10,7 +10,7 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         #'file:/pool/ciencias/userstorage/vrbouza/ntuples/DT_ntuples/2018/Prompt/DTTree_SingleMuon_ZMuSkim315252-315510.root'
         #'file:/pool/ciencias/userstorage/vrbouza/ntuples/DT_ntuples/simulaciones/longevity_task_force/CMSSW_10_2_5/src/UserCode/DTDPGAnalysis/test/SingleMuPt_3_1000_Eta_1_24_N1M/SingleMuPt_3_1000_Eta_1_24_N1M_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM_0.root'
-        'file:/pool/cienciasrw/userstorage/vrbouza/ntuples/DT_ntuples/simulaciones/MuonGuns/CMSSW_10_1_1/src/UserCode/producers/Pt2_100_Eta1_24_B2B/step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM_5.root'
+        'file:/pool/ciencias/userstorage/vrbouza/ntuples/DT_ntuples/simulaciones/MuonGuns/CMSSW_10_1_1/src/UserCode/producers/Pt2_100_Eta1_24_B2B/step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM_5.root'
     )
 )
 
